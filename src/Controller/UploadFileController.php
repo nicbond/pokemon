cf423a7db59c2a2e8b033a75edf9871c4666d093
nicbond <nicolasmartins78650@free.fr>
2021-07-27 11:32:24 +0200

    Add a microservice allowing only the upload of csv file

---
+++ src/Controller/UploadFileController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Service\FileUploader;
+use App\Service\Validator;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class UploadFileController extends AbstractController
+{
+    /**
+     * @param Request $request
+     * @param string $uploadDir
+     * @param FileUploader $uploader
+     * @param Validator $validator
+     * @param SerializerInterface $serializer
+     * @return Response
+     * @throws \Exception
+     */
+    #[Route('/upload', name: 'upload')]
+    public function index(Request $request, string $uploadDir, FileUploader $uploader, Validator $validator, SerializerInterface $serializer): Response
+    {
+        //Date du jour
+        $date = new \DateTime();
+        $today = $date->format('Y-m-d');
+
+        /** @var UploadedFile $uploadedFile */
+        $file = $request->files->get('attachment');
+
+        //Vérification du format du fichier autorisé et retour des erreurs éventuelles
+        $key = $validator->validatorFile($file, $request);
+
+        //Nouveau nom du fichier avec la date du jour
+        $extensionFile = $key['extensionFile'];
+        $newFilename = $today . '.' . $extensionFile;
+
+        //Appel du service FileUploader
+        $uploader->upload($uploadDir, $file, $newFilename);
+
+        //Si pas d'erreur via le validator, affichage du message d'upload
+        $data[] = ['message' => 'CSV uploaded'];
+        return new Response($serializer->serialize($data, 'json', []));
+    }
+}
---
+++ src/Service/Validator.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class Validator
+{
+    public function validatorFile($file, $request): array
+    {
+        $data = array();
+        $fileExtension = '';
+
+        //S'il n'y a pas de fichier uploadé
+        if (empty($file)) {
+            throw new \Exception('File upload is required', Response::HTTP_UNPROCESSABLE_ENTITY);
+        } else {
+            $fileExtension = $file->guessExtension();
+
+            //Si le fichier uploadé est différent du format csv
+            if ($fileExtension != 'csv') {
+                throw new \Exception('CSV file are only allowed', Response::HTTP_BAD_REQUEST);
+            } else {
+                $data['extensionFile'] = $file->guessExtension();
+            }
+        }
+        return $data;
+    }
+}
