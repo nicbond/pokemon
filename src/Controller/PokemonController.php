85b2548226525a00b378c4b0c2125f9ed495096e
nicbond <nicolasmartins78650@free.fr>
2021-07-29 19:32:29 +0200

    Add method show for retrieves a Pokemon resource by his id

---
+++ src/Controller/PokemonController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Pokemon;
+use App\Repository\PokemonRepository;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Serializer\SerializerInterface;
+
+class PokemonController extends AbstractController
+{
+     /**
+     * @var PokemonRepository;
+     */
+    private $repository;
+     /**
+     * @var SerializerInterface;
+     */
+    private $serializer;
+
+    public function __construct(PokemonRepository $repository, SerializerInterface $serializer)
+    {
+        $this->repository = $repository;
+        $this->serializer = $serializer;
+    }
+
+    #[Route('/api/pokemon/{id}', name: 'api_pokemon_get_item')]
+    public function show($id)
+    {
+        $pokemon = $this->repository->find($id);
+
+        if (empty($pokemon)){
+            throw new \Exception('The resource does not exist', Response::HTTP_BAD_REQUEST);
+        }
+        return $pokemon;
+    }
+}
