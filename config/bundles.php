137d99f946273c9b5618ead9f1064fdbd3df70fc
nicbond <nicolasmartins78650@free.fr>
2021-07-29 10:54:50 +0200

    Installation of the Symfony profiler to allow debugging

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
